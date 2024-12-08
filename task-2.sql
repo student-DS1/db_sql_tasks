a1b242c05f42adafcf7f6763acc0080cfaa3f98f
student-DS1 <d.saharov@istu.edu.ua>
2024-12-08 15:09:52 +0200

    Практическая работа 2: SQL-запросы

---
+++ task-2.sql
@@ -0,0 +1,59 @@
+Режим вывода команд на экран (ECHO) включен.
+
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    duration INTEGER NOT NULL,
+    category_id INTEGER REFERENCES categories(id)
+);
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255),
+    city VARCHAR(100)
+);
+
+CREATE TABLE rentals (
+    id SERIAL PRIMARY KEY,
+    movie_id INTEGER REFERENCES movies(id),
+    customer_id INTEGER REFERENCES customers(id),
+    rental_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+SELECT m.title, m.duration, c.name AS category
+FROM movies m
+JOIN categories c ON m.category_id = c.id;
+
+SELECT m.title, r.rental_date
+FROM rentals r
+JOIN movies m ON r.movie_id = m.id
+WHERE r.customer_id = 1; -- ╟рьхэшЄх `1` эр ID ъышхэЄр
+
+
+SELECT m.title, COUNT(r.id) AS rental_count
+FROM rentals r
+JOIN movies m ON r.movie_id = m.id
+GROUP BY m.title
+ORDER BY rental_count DESC
+LIMIT 5;
+
+INSERT INTO customers (name, address, city)
+VALUES ('Alice Cooper', '123 Main St', 'San Francisco');
+
+UPDATE customers
+SET address = '456 Elm St'
+WHERE name = 'Alice Cooper';
+
+DELETE FROM customers
+WHERE name = 'Alice Cooper';
+
+
+
+
